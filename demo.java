1e30d085990ee2aa054f55588a65c91687970c54
Riyad <riyadgit@gmail.com>
2018-05-16 15:29:33 +0300

    Last Version1

--- demo.java
+++ demo.java
@@ -3,7 +3,8 @@ class Hello{
 	public static void main(string arg[]){
    		system.out.println("");
 		system.out.println("");
-		system.out.println("Good")
+		system.out.println("Good");
+           system.out.println("Get the number");   
 }
 
 } 
